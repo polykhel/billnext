786aa8040ca85ffed73330be82403a85e3350329
Michael Pollente <mpollente@exist.com>
2021-03-14 15:06:16 +0800

    Initial version of billies generated by JHipster-7.0.0-beta.1

---
+++ src/main/java/com/polykhel/billies/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.polykhel.billies.repository;
+
+import com.polykhel.billies.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
