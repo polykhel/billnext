d1548ed4bf5d1285699202dbaa8c7f12656903a3
Michael Pollente <mpollente@exist.com>
2021-05-14 20:42:13 +0800

    Add reset password finish

--- src/main/webapp/app/account/account.module.ts
+++ src/main/webapp/app/account/account.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { accountState } from 'app/account/account.route';
+import { PasswordResetFinishComponent } from 'app/account/password-reset/finish/password-reset-finish.component';
 import { PasswordResetInitComponent } from 'app/account/password-reset/init/password-reset-init.component';
 import { PasswordStrengthBarComponent } from 'app/account/password/password-strength-bar/password-strength-bar.component';
 import { PasswordComponent } from 'app/account/password/password.component';
@@ -11,7 +12,13 @@ import { ActivateComponent } from './activate/activate.component';
 @NgModule({
   imports: [SharedModule, RouterModule.forChild(accountState)],
   exports: [],
-  declarations: [ActivateComponent, PasswordComponent, PasswordStrengthBarComponent, PasswordResetInitComponent],
+  declarations: [
+    ActivateComponent,
+    PasswordComponent,
+    PasswordStrengthBarComponent,
+    PasswordResetInitComponent,
+    PasswordResetFinishComponent,
+  ],
   providers: [],
 })
 export class AccountModule {}
--- src/main/webapp/app/account/account.route.ts
+++ src/main/webapp/app/account/account.route.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { passwordResetFinishRoute } from 'app/account/password-reset/finish/password-reset-finish.route';
 import { passwordResetInitRoute } from 'app/account/password-reset/init/password-reset-init.route';
 import { passwordRoute } from 'app/account/password/password.route';
 
 import { activateRoute } from './activate/activate.route';
 
-const ACCOUNT_ROUTES = [activateRoute, passwordRoute, passwordResetInitRoute];
+const ACCOUNT_ROUTES = [activateRoute, passwordRoute, passwordResetInitRoute, passwordResetFinishRoute];
 
 export const accountState: Routes = [
   {
